7e9bd20eba5052241d0ab195a1f6c5662a9372f3
Big-pun <150970603+Big-pun@users.noreply.github.com>
2024-08-06 11:30:41 -0400

    abonnements adultes

--- scripts/navbar.js
+++ scripts/navbar.js
@@ -1,9 +1,31 @@
 // Function to toggle the mobile menu
 function toggleMobileMenu() {
-    const navbar = document.getElementById('navbar-sticky');
-    navbar.classList.toggle('hidden');
+  const navbar = document.getElementById("navbar-sticky");
+  navbar.classList.toggle("hidden");
 }
 
 // Event listener for mobile menu button
-const mobileMenuButton = document.querySelector('[data-collapse-toggle="navbar-sticky"]');
-mobileMenuButton.addEventListener('click', toggleMobileMenu);
+const mobileMenuButton = document.querySelector(
+  '[data-collapse-toggle="navbar-sticky"]'
+);
+mobileMenuButton.addEventListener("click", toggleMobileMenu);
+
+// Dropdown menu abonnements in navbar
+document.addEventListener("DOMContentLoaded", function () {
+  const dropdownToggle = document.getElementById("dropdownNavbarLink");
+  const dropdownMenu = document.getElementById("dropdownNavbar");
+
+  dropdownToggle.addEventListener("click", function () {
+    dropdownMenu.classList.toggle("hidden");
+  });
+
+  // Optional: Close the dropdown if clicked outside
+  document.addEventListener("click", function (event) {
+    if (
+      !dropdownToggle.contains(event.target) &&
+      !dropdownMenu.contains(event.target)
+    ) {
+      dropdownMenu.classList.add("hidden");
+    }
+  });
+});
